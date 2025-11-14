e8f02a563554b16a147d2eb219565a41b781843d
Kent Bitanghol <kvbitanghol05181@liceo.edu.ph>
2025-11-14 23:37:14 +0800

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.karenderya {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.karenderya to javafx.fxml;
+    exports com.example.karenderya;
+}
